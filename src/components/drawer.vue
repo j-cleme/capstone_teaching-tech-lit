137d486934d0e8eae5d93816611a790d809cccbd
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2022-11-21 13:38:02 -0500

    Created sidenav component

---
+++ src/components/drawer.vue
@@ -0,0 +1,43 @@
+<script>
+    function closeNav(){
+        let sidenav = document.getElementById('sidenav');
+        sidenav.style.width = "0px";
+    }
+</script>
+<template>
+    <nav class="app-Drawer">
+        <div class="drawer-container" id="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+            <a href="#">Lorem Ipsum</a>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.drawer-container{
+    margin: 0;
+    padding: 0;
+    top: 0;
+    left: 0;
+    height: 100%;
+    z-index: 1;
+    position: fixed;
+    background-color: #111;
+    width: 0px;
+    transition: 0.5s;
+}
+
+.drawer-container a{
+    text-decoration: none;
+    display: block;
+    padding: 15px;
+    color: white;
+}
+
+.drawer-container a:hover{
+  background-color: #F76902;
+}
+</style>
