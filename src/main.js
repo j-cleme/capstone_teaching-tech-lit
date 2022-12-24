03f178606f0ba4b3a218b434594831c2a4626592
jcleme <clemensjosh@protonmail.com>
2022-12-24 17:37:38 -0500

    no more bootstrap, now is the time of tailwind
    bootstrap didn't have the utility class i was looking for (translate-x-full) so i swapped back

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
 import "./assets/main.css";
 
 createApp(App).mount("#app");
