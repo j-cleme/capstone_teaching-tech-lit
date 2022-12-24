493323b2e3ba2ee621ee4533c6d756a33b260c54
jcleme <clemensjosh@protonmail.com>
2022-12-24 17:41:14 -0500

    no

--- src/components/TeachingContainer.vue
+++ src/components/TeachingContainer.vue
@@ -40,31 +40,3 @@ export default {
     </div>
   </article>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 300;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-article {
-  border: 1px solid #ffffff;
-  border-radius: 5px;
-  padding: 2.5rem;
-  background-color: #7777774a;
-  display: grid;
-  grid-template-columns: 2fr 3fr;
-  gap: 2.5rem;
-}
-
-article * {
-  display: flex;
-  place-items: center;
-  place-content: center;
-}
-
-.iconSlot {
-  transform: scale(2.5);
-}
-</style>
