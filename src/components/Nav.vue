ef9ab27aec3dcb069bb9a0ce8c19d91ace618ef6
jcleme <clemensjosh@protonmail.com>
2022-12-27 15:18:01 -0500

    Create NavLink component

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Menu } from 'lucide-vue-next';
+import NavLink from './NavLink.vue'
 </script>
 
 <script>
@@ -26,25 +27,16 @@ export default {
   <!-- TODO this *should* be 2 components (header and nav) but i am lazy and sloppily combining 
     them is easy and fast. once split, toggleNav() needs to change to properly send info between components. -->
   <header class="bg-neutral-300 sticky w-full h-16 m-0 flex flex-row justify-start items-center">
-    <Menu :size="50" @click="toggleNav()" class="mx-2 my-2 md:hover:rotate-90 duration-75 text-neutral-600 lg:hidden" />
+    <Menu :size="50" @click="toggleNav()" class="mx-2 my-2 md:hover:rotate-90 duration-75 text-neutral-600 " />
     <h1 class="mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900">Teaching Tech Lit</h1>
   </header>
   <nav
     class="bg-neutral-200 grid grid-cols-1 grid-flow-row auto-rows-max justify-around h-screen duration-100 absolute z-10"
     :class="[navOpen ? 'w-3/4' : 'w-0']" id="sidenav">
-    <!-- <Menu :size="50" @click="toggleNav()" class="mx-2 my-2 md:hover:rotate-90 duration-75 text-neutral-600" /> -->
-    <a class="px-4 py-2 text-lg font-semibold text-neutral-900 duration-100" :class="[navOpen ? '' : 'hidden']"
-      href="#">Lorem
-      Ipsone</a>
-    <a class="px-4 py-2 text-lg font-semibold text-neutral-900 duration-100" :class="[navOpen ? '' : 'hidden']"
-      href="#">Lorem
-      Ipstwo</a>
-    <a class="px-4 py-2 text-lg font-semibold text-neutral-900 duration-100" :class="[navOpen ? '' : 'hidden']"
-      href="#">Lorem
-      Ipsthree</a>
-    <!-- TODO please dont type all these out manually lol -->
-    <!-- could use pug, or make another component specifically for this link, but 
-      honestly if we are just making this list once thats too much effort -->
-    <!-- ez emmet ebbreviation: a.px-5.py-2.text-lg.font-semibold.text-neutral-900 -->
+    <NavLink :nav-open="navOpen" :href="'/quiztest'">Test Quiz</NavLink>
+    <NavLink :nav-open="navOpen" :href="'/'">Lorem Ipsone</NavLink>
+    <NavLink :nav-open="navOpen" :href="'/'">Lorem Ipstwo</NavLink>
+    <NavLink :nav-open="navOpen" :href="'/'">Lorem Ipsthree</NavLink>
+    <NavLink :nav-open="navOpen" :href="'/'">Lorem Ipsfour</NavLink>
   </nav>
 </template>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {}
+  },
+  props: {
+    navOpen: Boolean,
+    href: { type: String, default() { return "#" } },
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <a class="px-4 py-2 text-lg font-semibold text-neutral-900 duration-100" :class="[navOpen ? '' : 'hidden']"
+    :href="href">
+    <slot>Lorem Ipsum</slot>
+  </a>
+</template>
