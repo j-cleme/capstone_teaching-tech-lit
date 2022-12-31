70d4a65968e8b42e7bac999db143a8c79d435ab0
jcleme <clemensjosh@protonmail.com>
2022-12-31 13:11:38 -0500

    fixed section generation

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@ export default {
   data() {
     return {
       sections: [
-        { title: "Menu Icon", icon: "Menu", textContent: "This is a menu icon, sometimes called a \"Hamburger Menu\" due to its horizontal layers." },
-        { title: "Search Icon", icon: "Search", textContent: "This is a search icon, indicating a searchable field." },
-        { title: "Bean Icon", icon: "BeanOff", textContent: "This indicates that your bean has been disabled." }
+        { title: "Menu Icon", icon: Menu, textContent: "This is a menu icon, sometimes called a \"Hamburger Menu\" due to its horizontal layers." },
+        { title: "Search Icon", icon: Search, textContent: "This is a search icon, indicating a searchable field." },
+        { title: "Bean Icon", icon: BeanOff, textContent: "This indicates that your bean has been disabled." }
       ]
     };
   },
@@ -23,14 +23,7 @@ export default {
 <template>
   <Nav />
   <main class="flex flex-col justify-center">
-    <TeachingContainer :title="sections[0].title" :text-content="sections[0].textContent">
-      <Menu />
-    </TeachingContainer>
-    <TeachingContainer :title="sections[1].title" :text-content="sections[1].textContent">
-      <Search />
-    </TeachingContainer>
-    <TeachingContainer :title="sections[2].title" :text-content="sections[2].textContent">
-      <BeanOff />
-    </TeachingContainer>
+    <TeachingContainer v-for="item in sections" :title="item.title" :icon="item.icon"
+      :text-content="item.textContent" />
   </main>
 </template>
--- src/components/TeachingContainer.vue
+++ src/components/TeachingContainer.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { VueElement } from 'vue';
+
 
 </script>
+
 <script>
 
 export default {
@@ -9,6 +12,7 @@ export default {
   },
   props: {
     title: String,
+    icon: VueElement,
     textContent: String,
   },
   methods: {
@@ -21,6 +25,13 @@ export default {
 }
 </script>
 
+<!-- <BlogPost
+  v-for="post in posts"
+  :key="post.id"
+  :title="post.title"
+ /> -->
+
+
 <template>
   <article class="border border-neutral-600 p-4 m-4 grid grid-cols-2 grid-rows-2 items-center justify-center gap-2"
     @click="clickHandler(title)">
@@ -30,7 +41,6 @@ export default {
     <p class="row-span-2">
       {{ textContent }}
     </p>
-    <slot>
-    </slot>
+    <component :is="icon"></component>
   </article>
 </template>
