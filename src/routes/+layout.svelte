1974c961c8641435cbafb03aa059171781228f10
jcleme <clemensjosh@protonmail.com>
2022-12-31 19:09:43 -0500

    SvelteKit!
    switched the whole thing from vue to svelte. solves a whole bunch of problems (svg imports, routing, composable layouts) in a much more maintainable way

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script>
+	import '../app.css';
+	import { Menu } from 'lucide-svelte';
+
+	let navOpen = false;
+	let navBaseStyles =
+		'bg-neutral-200 grid grid-cols-1 grid-flow-row auto-rows-max justify-around h-screen duration-100 absolute z-10 ';
+	let navStyleOpen = 'w-3/4';
+	let navStyleClosed = 'w-0';
+	let navStyles;
+
+	let navLinkBaseStyle = 'px-4 py-2 text-lg font-semibold text-neutral-900 duration-100 ';
+	let hiddenStyle = 'hidden';
+	let navLinkStyle = navLinkBaseStyle + hiddenStyle;
+	if (navOpen) {
+	} else {
+	}
+
+	function toggleNav() {
+		// let sidenav = document.getElementById('sidenav');
+		console.log('toggleNav got called');
+		switch (navOpen) {
+			case true:
+				navOpen = false;
+				navStyles = navBaseStyles + navStyleClosed;
+				navLinkStyle = navLinkBaseStyle + hiddenStyle;
+				console.log('set navOpen to false');
+				break;
+
+			case false:
+				navOpen = true;
+				navStyles = navBaseStyles + navStyleOpen;
+				navLinkStyle = navLinkBaseStyle;
+				console.log('set navOpen to true');
+				break;
+		}
+	}
+</script>
+
+<header class="bg-neutral-300 sticky w-full h-16 m-0 flex flex-row justify-start items-center">
+	<div on:click={toggleNav} on:keypress={toggleNav}>
+		<Menu size="50" class="mx-2 my-2 md:hover:rotate-90 duration-75 text-neutral-600 " />
+	</div>
+	<h1 class="mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900">Teaching Tech Lit</h1>
+</header>
+<nav class={navStyles} id="sidenav">
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Home </a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/quiztest" class={navLinkStyle}
+		>quiztest
+	</a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Home2 </a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Lorem Ipsone </a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Lorem Ipstwo </a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Lorem Ipsthree </a>
+	<a on:click={toggleNav} on:keypress={toggleNav} href="/" class={navLinkStyle}>Lorem Ipsfour </a>
+</nav>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import TeachingContainer from './TeachingContainer.svelte';
+	import { Menu, Search, BeanOff } from 'lucide-svelte';
+
+	let sections = [
+		{
+			title: 'Menu Icon',
+			icon: Menu,
+			textContent:
+				'This is a menu icon, sometimes called a "Hamburger Menu" due to its horizontal layers.'
+		},
+		{
+			title: 'Search Icon',
+			icon: Search,
+			textContent: 'This is a search icon, indicating a searchable field.'
+		},
+		{
+			title: 'Bean Icon',
+			icon: BeanOff,
+			textContent: 'This indicates that your bean has been disabled.'
+		}
+	];
+</script>
+
+<main class="flex flex-col justify-center">
+	{#each sections as item}
+		<TeachingContainer title={item.title} icon={item.icon} textContent={item.textContent} />
+	{/each}
+</main>
