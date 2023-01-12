728908e29fc002564d192b81d924d4b03fdcd9a9
jcleme <clemensjosh@protonmail.com>
2023-01-12 14:45:32 -0500

    create example quiz page

---
+++ src/routes/quiztest/+page.svelte
@@ -0,0 +1,21 @@
+<script></script>
+
+<main class="flex flex-col justify-center m-5 mx-auto justify-self-center md:w-1/2 border p-4">
+	<section class="">
+		<h2 class="text-lg font-semibold">Question 1</h2>
+		<select multiple name="q1" id="q1" class="flex flex-row justify-center items-center">
+			<select class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
+				>this one is right</select
+			>
+			<option class="p-4 bg-neutral-50 border border-neutral-900" value="wrong"
+				>this one is wrong</option
+			>
+			<option class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
+				>this one is also right</option
+			>
+			<option class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
+				>this one might be right</option
+			>
+		</select>
+	</section>
+</main>
