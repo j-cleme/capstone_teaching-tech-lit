c370b2f285ec9a81f8be89bead7486e54d41ecd9
jcleme <clemensjosh@protonmail.com>
2023-01-13 20:57:33 -0500

    automatically create quiz questions from template
    who doesn't love a lil automatic generation?

--- src/routes/quiztest/+page.svelte
+++ src/routes/quiztest/+page.svelte
@@ -1,21 +1,37 @@
-<script></script>
+<script>
+	export let quiz = [
+		{
+			question: 'Which of these options is correct?',
+			options: ['one', 'two', 'three', 'four'],
+			answers: ['one', 'four']
+		},
+		{
+			question: 'a very well thought out second question',
+			options: ['good answer', 'bad answer', 'not good enough answer'],
+			answers: ['good answer']
+		}
+	];
+</script>
 
 <main class="flex flex-col justify-center m-5 mx-auto justify-self-center md:w-1/2 border p-4">
-	<section class="">
-		<h2 class="text-lg font-semibold">Question 1</h2>
-		<select multiple name="q1" id="q1" class="flex flex-row justify-center items-center">
-			<select class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
-				>this one is right</select
-			>
-			<option class="p-4 bg-neutral-50 border border-neutral-900" value="wrong"
-				>this one is wrong</option
-			>
-			<option class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
-				>this one is also right</option
-			>
-			<option class="p-4 bg-neutral-50 border border-neutral-900" value="correct"
-				>this one might be right</option
-			>
-		</select>
-	</section>
+	{#each quiz as question}
+		<section class="min-w-full bg-neutral-200 p-4 m-4">
+			<h2 class="text-lg font-semibold">Question 1</h2>
+			{#each question.options as option}
+				<form
+					multiple
+					name="q1"
+					id="q1"
+					class="flex flex-col justify-center items-center min-w-full"
+				>
+					<label for="option">{option}</label>
+					<input
+						type="radio"
+						id={option}
+						class="p-8 bg-neutral-500 border border-neutral-900 min-w-full"
+					/>
+				</form>
+			{/each}
+		</section>
+	{/each}
 </main>
