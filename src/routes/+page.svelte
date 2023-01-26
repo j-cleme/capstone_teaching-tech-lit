f3f812e5689228b3a778226bdf6ed5fe7c2ddb6e
jcleme <clemensjosh@protonmail.com>
2023-01-26 18:11:17 -0500

    started on button lesson
    based on new curriculum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,45 @@
 </script>
 
 <main class="flex flex-col justify-center m-5 mx-auto justify-self-center md:w-1/2 border p-4">
+	<article>
+		<h1 class="text-5xl leading-normal">Buttons</h1>
+		<p>Buttons can look like anything</p>
+		<button
+			class="m-5 p-5 bg-teal-400 border-4 border-teal-400 active:border-4 active:border-neutral-800"
+			>click me!</button
+		>
+		<button
+			class="m-5 p-5 bg-teal-800 text-white rounded-2xl border-4 border-teal-800 active:border-4 active:border-neutral-800"
+			>click me!</button
+		>
+		<button
+			class="m-5 p-2 bg-orange-400 rounded-md border-4 border-orange-400 active:border-4 active:border-neutral-800"
+			>click me!</button
+		>
+		<button
+			class="m-5 px-8 p-2 bg-cyan-400 border-4 border-cyan-400 active:border-4 active:border-neutral-800 rounded"
+			>click me!</button
+		>
+		<button
+			class="m-5 p-4 bg-rose-600 decoration-teal-900 border-4 border-rose-600 active:border-4 active:border-neutral-800"
+			disabled>don't click this one</button
+		>
+		<p>Buttons sometimes come in collections.</p>
+		<span
+			class="w-full m-2 p-2 border border-solid border-blue-700 flex flex-row justify-around items-center"
+		>
+			<button
+				class="p-2 bg-neutral-700 text-white w-full border border-white active:border-teal-400"
+				>one</button
+			>
+			<button
+				class="p-2 bg-neutral-700 text-white w-full border border-white active:border-teal-400"
+				>two</button
+			>
+			<button
+				class="p-2 bg-neutral-700 text-white w-full border border-white active:border-teal-400"
+				>three</button
+			>
+		</span>
+	</article>
 </main>
