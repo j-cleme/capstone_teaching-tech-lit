82fa5378c8db5cf5646906a5513a3214b9349078
jcleme <clemensjosh@protonmail.com>
2023-01-26 18:10:47 -0500

    removed old template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,5 @@
 <script>
-	import TeachingContainer from './TeachingContainer.svelte';
-	import { Menu, Search, BeanOff } from 'lucide-svelte';
-
-	let sections = [
-		{
-			title: 'Menu Icon',
-			icon: Menu,
-			textContent:
-				'This is a menu icon, sometimes called a "Hamburger Menu" due to its horizontal layers.'
-		},
-		{
-			title: 'Search Icon',
-			icon: Search,
-			textContent: 'This is a search icon, indicating a searchable field.'
-		},
-		{
-			title: 'Bean Icon',
-			icon: BeanOff,
-			textContent: 'This indicates that your bean has been disabled.'
-		}
-	];
 </script>
 
 <main class="flex flex-col justify-center m-5 mx-auto justify-self-center md:w-1/2 border p-4">
-	{#each sections as item}
-		<TeachingContainer {...item} />
-	{/each}
 </main>
