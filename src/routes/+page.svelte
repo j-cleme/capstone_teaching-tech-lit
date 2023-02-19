ac95dbcc09562bd51c4be308228fb1b9e06b748b
jcleme <clemensjosh@protonmail.com>
2023-02-18 21:53:10 -0500

    first pass at landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script>
 </script>
 
+<main
+	class="flex flex-row w-full md:w-3/4 m-auto p-12 basis-1/2 items-center justify-center gap-24"
+>
+	<div>
+		<h1 class="text-6xl leading-normal font-bold">Teaching Tech Lit</h1>
+		<h2 class="text-2xl leading-normal font-semibold">A guided introduction to the internet</h2>
 		<br />
+		<a
+			href="/buttons"
+			class="text-black bg-gradient-to-br from-teal-500 to-cyan-400 text-2xl duration-75 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-semibold rounded-lg text-md px-5 py-2.5 text-center"
+			>Get Started &#10140;</a
+		>
+	</div>
+	<div><img src="/assets/logo.png" alt="the Teaching Tech Lit logo." /></div>
 </main>
