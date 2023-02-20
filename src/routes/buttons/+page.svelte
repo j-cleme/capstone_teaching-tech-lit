832070a55ab07835cb892a9be4822eb454c23805
jcleme <clemensjosh@protonmail.com>
2023-02-20 18:51:18 -0500

    removed duplicate css

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -41,7 +41,7 @@
 			class="transition duration-100 ease-in-out relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
 		>
 			<span
-				class="transition duration-100 ease-in-out relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0"
+				class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0"
 			>
 				Pink to orange
 			</span>
