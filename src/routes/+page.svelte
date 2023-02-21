a80b82c4a34d94e99b0f5c1ca713c2770830bdcf
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-02-21 14:17:28 -0500

    Updated Page
    Playing with layouts, added about text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,36 @@
 <script>
 </script>
-
+<style>
+	main{
+		font-family: 'Helvetica', sans-serif;
+		font-size: 20px;
+	}
+</style>
 <main
-	class="flex flex-row w-full md:w-3/4 m-auto p-12 basis-1/2 items-center justify-center gap-24"
+	class="px-24"
 >
-	<div>
+<div class="grid lg:grid-cols-2 px-24">
+	<div class="flex flex-col flex-wrap justify-center">
+		<h1 class="text-6xl leading-normal font-bold">Teaching Tech Lit</h1>
+		<h2 class="text-2xl leading-normal font-semibold">A guided introduction to the internet</h2>
+		<a
+			href="/buttons"
+			class="text-white bg-blue-500 text-2xl duration-75 focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 w-60 font-semibold rounded-lg text-md px-5 py-2.5 text-center"
+			>Get Started &#10140;</a
+		>
+	</div>
+	<div class="flex flex-wrap flex-col content-center"><img src="/assets/logo.png" alt="the Teaching Tech Lit logo." /></div>
+</div>
+<hr class="pb-10">
+<div class="grid lg:grid-cols-1 px-24">
+	<h2 class="text-5xl leading-normal font-bold">What is Teaching Tech Lit?</h2>
+	<div class="pr-16">
+		<p>
+			Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.
+		</p>
+	</div>
+</div>
+	<!--<div>
 		<h1 class="text-6xl leading-normal font-bold">Teaching Tech Lit</h1>
 		<h2 class="text-2xl leading-normal font-semibold">A guided introduction to the internet</h2>
 		<br />
@@ -15,5 +41,5 @@
 		>
 	</div>
 	<div><img src="/assets/logo.png" alt="the Teaching Tech Lit logo." /></div>
-	<hr/>
+-->
 </main>
