b66180d61295f9b909316a7bd83bf4ef8a40da7d
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-02-21 16:48:58 -0500

    Updates to Page
    Added additional layout changes, added about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<main class="flex flex-col md:flex-row md:basis-2/4 md:justify-center md:gap-16">
+<!--<main class="flex flex-col md:flex-row md:basis-2/4 md:justify-center md:gap-16">
 	<div
 		class="mt-4 md:mt-0 h-80 flex flex-col justify-around items-center md:items-start gap-y-3 md:gap-y-0"
 	>
@@ -22,4 +22,39 @@
 		<img src="/assets/logo.png" alt="the Teaching Tech Lit logo." />
 	</div>
 	<hr />
-</main>-->
+</main>
+-->
+
+<main>
+	<div class="grid lg:grid-cols-2">
+		<div class="flex flex-col justify-center leading-loose justify-items-center">
+			<h1 class="text-6xl md:text-6xlmd:text-left font-semibold pb-4">
+				Teaching Tech Lit
+			</h1>
+			<h2 class="text-2xl md:text-left pb-4 ">
+				A guided introduction to the internet
+			</h2>
+			<button class="bg-blue-500  text-white font-semibold py-2 px-4 border  rounded w-52 text-2xl ">
+				Get Started &#10140;
+			</button>
+			 <!-- <a 
+				href="/buttons"
+				class="scale-150 scale:0 justify-self-end text-black bg-gradient-to-br from-teal-500 to-cyan-400 text-2xl duration-75 drop-shadow-md hover:drop-shadow-lg focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-semibold rounded-lg text-md px-5 py-2.5 text-center"
+				>Get Started &#10140;</a
+			>-->
+		</div>
+		<div class="flex flex-col justify-center items-center">
+			<img src="/assets/logo.png" alt="the Teaching Tech Lit logo." />
+		</div>
+	</div>
+	<hr/>
+	<div class="flex flex-col my-10">
+		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold pb-4">
+			What is Teaching Tech Lit?
+		</h1>
+
+		<p class="text-lg">
+			Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.
+		</p>
+	</div>
+</main>
