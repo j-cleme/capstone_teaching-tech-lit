b1afc3075975746fa55c4465317d0a5fd59f87a3
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-02-22 21:01:45 -0500

    Finished layout of homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
-<div class="test">
+<div class="hero">
 	<div class="grid lg:grid-cols-2 content-area">
 		<div class="flex flex-col justify-center leading-loose justify-items-center px-4 text-white">
 			<h1 class="text-6xl md:text-6xlmd:text-left font-semibold pb-4">
@@ -23,20 +23,59 @@
 		</div>
 	</div>
 </div>
-<hr/>
 <div class="content-area">
-	<div class="flex flex-col my-10">
-		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold pb-3 border-b w-98" style="color: #3549A6;">
-			What is Teaching Tech Lit?
-		</h1>
-
-		<div class="mr-32">
-			<p class="text-xl font-thin pt-3">
-				Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.
-			</p>
+	<div class="py-16">
+		<div class="flex flex-col">
+			<h1 class="text-5xl md:text-6xlmd:text-left font-semibold pb-3 border-b w-98" style="color: #3549A6;">
+				What is Teaching Tech Lit?
+			</h1>
+	
+			<div class="mr-32">
+				<p class="text-xl font-thin pt-3">
+					Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.
+				</p>
+			</div>
 		</div>
 	</div>
+	<hr/>
+	<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mt-16 pb-3" style="color: #3549A6;">
+		Lessons
+		<hr class="w-56 mt-3"/>
+	</h1>
+	<div class="grid lg:grid-cols-3 mt-10 mb-10">
+		<div class="cards rounded overflow-hidden shadow border py-2">
+			<div class="">
+			  <div class="font-bold text-3xl py-4 mb-2 text-gray-800">Browsers</div>
+			  <img src="/assets/Edge_logo.png" alt="edge logo" class="responsive pb-4"/>
+			</div>
+			<div class="py-2">
+			  <span class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700">Not Completed</span>
+			</div>
+			<button class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600">Start</button>
+		  </div>
+		  <div class="cards rounded overflow-hidden shadow border py-2">
+			<div class="">
+			  <div class="font-bold text-3xl py-4 mb-2 text-gray-800">Browsers</div>
+			  <img src="/assets/Edge_logo.png" alt="edge logo" class="responsive pb-4"/>
+			</div>
+			<div class="py-2">
+			  <span class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700">Not Completed</span>
+			</div>
+			<button class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600">Start</button>
+		  </div>
+		  <div class="cards rounded overflow-hidden shadow border py-2">
+			<div class="">
+			  <div class="font-bold text-3xl py-4 mb-2 text-gray-800">Browsers</div>
+			  <img src="/assets/Edge_logo.png" alt="edge logo" class="responsive pb-4"/>
+			</div>
+			<div class="py-2">
+			  <span class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700">Not Completed</span>
+			</div>
+			<button class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600">Start</button>
+		  </div>
+	</div>
 </div>
+
 <!--<main class="flex flex-col md:flex-row md:basis-2/4 md:justify-center md:gap-16">
 	<div
 		class="mt-4 md:mt-0 h-80 flex flex-col justify-around items-center md:items-start gap-y-3 md:gap-y-0"
