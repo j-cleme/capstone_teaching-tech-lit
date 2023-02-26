37b8aa172693671584f873d0b4dbb8b953079569
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-02-26 17:33:42 -0500

    Update +page.svelte

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -1,3 +1,45 @@
-<main>
-	<h1 id="Devices">Devices</h1>
-</main>
+<div class="bg-[#3549A6]">
+	<div class="content-area">
+		<h1 class="text-white" id="Icons">Lesson - Devices</h1>
+	</div>
+</div>
+<div class="content-area">
+	<div class="mt-10 mb-4">
+		<p class="text-xl font-thin">
+			All devices are different, but some functions are seen across all platforms. Almost all devices have a top and/or bottom bar, these allow you to access things you need often quickly and easily.
+		</p>
+		<br/>
+	<p class="text-xl font-thin">For Apple there is both a top and bottom bar, the top bar is a menu and the bottom bar is used to hold your computer's apps:</p>
+	<br />
+	<p class="text-xl font-thin">Windows only has a bottom bar that holds your open and most used apps as well as the start button, which we will expand on.</p>
+
+	</div>
+	<hr>
+	<div class="mt-10 mb-4">
+		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mb-2" style="color: #3549A6;">
+			Start Buttons
+		</h1>
+		<p class="text-xl font-thin mt-4 mb-6">
+			A start button can be used to search a device for programs or files, its look and location differ depending on device.
+		</p>
+		<div
+		class=" m-auto inline-grid grid-cols-1 lg:grid-cols-3 grid-rows-3 lg:grid-rows-1 items-center justify-around my-4"
+	>
+		<div class="cards rounded overflow-hidden shadow border py-2 px-2 h-full">
+			<div class="">
+			<div class="font-bold text-3xl mx-2 py-4 mb-2 text-gray-800">MacOS</div>
+			</div>
+			<div class="pt-2 pb-4">
+			</div>
+		</div>
+		<div class="cards rounded overflow-hidden shadow border py-2 px-2 h-full">
+			<div class="">
+			<div class="font-bold text-3xl py-4 mb-2 text-gray-800">Windows</div>
+			</div>
+			<div class="pt-2 pb-4">
+
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
