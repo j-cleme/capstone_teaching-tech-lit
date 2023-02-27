6b8df1580b4d23638c89fba9b10d352d500b4bad
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-02-27 02:18:51 -0500

    Updates to the devices page

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -4,14 +4,33 @@
 	</div>
 </div>
 <div class="content-area">
-	<div class="mt-10 mb-4">
+	<div class="mt-10 mb-16">
 		<p class="text-xl font-thin">
 			All devices are different, but some functions are seen across all platforms. Almost all devices have a top and/or bottom bar, these allow you to access things you need often quickly and easily.
 		</p>
 		<br/>
-	<p class="text-xl font-thin">For Apple there is both a top and bottom bar, the top bar is a menu and the bottom bar is used to hold your computer's apps:</p>
-	<br />
-	<p class="text-xl font-thin">Windows only has a bottom bar that holds your open and most used apps as well as the start button, which we will expand on.</p>
+		<div class="grid grid-cols lg:grid-cols-2">
+			<div class="m-auto">
+				<div class="cards rounded overflow-hidden shadow border p-6 h-full">
+					<p class="text-xl font-semibold">For Apple there is both a top and bottom bar, the top bar is a menu and the bottom bar is used to hold your computer's apps:</p>
+				<br />
+				<img src="/assets/MacOS_desktop.jpg" alt="MacOS Desktop" class="m-auto" />
+				<p class="mt-2 text-sm font-semibold">MacOS - Desktop</p>
+				</div>
+				
+			</div>
+
+			<div class="m-auto">
+				<div class="cards rounded overflow-hidden shadow border p-6 h-full">
+					<p class="text-xl font-semibold">Windows only has a bottom bar that holds your open and most used apps as well as the start button, which we will expand on.</p>				<br />
+				<img src="/assets/windows_desktop.png" alt="Windows Desktop" class="m-auto" />
+				<p class="mt-2 text-sm font-semibold">Windows 11 - Desktop</p>
+				</div>
+				
+			</div>
+
+		</div>
+	
 
 	</div>
 	<hr>
@@ -22,25 +41,33 @@
 		<p class="text-xl font-thin mt-4 mb-6">
 			A start button can be used to search a device for programs or files, its look and location differ depending on device.
 		</p>
-		<div
-		class=" m-auto inline-grid grid-cols-1 lg:grid-cols-3 grid-rows-3 lg:grid-rows-1 items-center justify-around my-4"
-	>
-		<div class="cards rounded overflow-hidden shadow border py-2 px-2 h-full">
-			<div class="">
-			<div class="font-bold text-3xl mx-2 py-4 mb-2 text-gray-800">MacOS</div>
-			</div>
-			<div class="pt-2 pb-4">
-				<img src="/assets/apple_logo.png" alt="Apple logo" class="responsive" />
-			</div>
-		</div>
-		<div class="cards rounded overflow-hidden shadow border py-2 px-2 h-full">
-			<div class="">
-			<div class="font-bold text-3xl py-4 mb-2 text-gray-800">Windows</div>
-			</div>
-			<div class="pt-2 pb-4">
-				<img src="/assets/windows_logo.png" alt="Windows logo" class="responsive" />
-			</div>
-		</div>
+		<div></div>
+
 	</div>
-	</div>
+	<hr>
+	<div class="mt-10 mb-16">
+		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mb-2" style="color: #3549A6;">
+			File Explorers
+		</h1>
+		<p class="text-xl font-thin mt-4 mb-6">
+			Every device will have a file explorer that allows you to search for and organize your files.		
+		</p>
+		<div class="grid grid-cols lg:grid-cols-2">
+			<div class="cards rounded overflow-hidden shadow border p-6 h-full">
+				<p class="text-xl font-thin mt-4 mb-6">
+					On MacOS you have Finder which looks like this:		
+				</p>
+				<img src="assets/Finder.png" alt="MacOS Finder">
+				<p class="text-sm font-semibold">MacOS Ventura - Finder</p>
+			</div>
+			<div class="cards rounded overflow-hidden shadow border p-6 h-full">
+				<p class="text-xl font-thin mt-4 mb-6">
+				On Windows you have File Explorer which looks like this:		
+			</p>
+			<img src="assets/File_Explorer.png" alt="Windows File Explorer" class="">
+			<p class="text-sm font-semibold mt-8">Windows 11 - File Explorer</p>
+		</div>
+		</div>
+		
+		</div>
 </div>
