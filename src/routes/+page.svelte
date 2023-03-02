f355c4712e3bf75ebb4fb3d3d53e41620c88dc60
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-02 12:00:08 -0500

    Landing page experimenting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,101 +1,26 @@
-<script>
-	import { MousePointer, Settings } from 'lucide-svelte';
-</script>
+<main>
 
-<!-- Hero/Landing section -->
-<div class="bg-[#3549A6]">
-	<div class="grid lg:grid-cols-2 content-area">
-		<div class="flex flex-col justify-center leading-loose justify-items-center px-4 text-white">
-			<h1 class="text-6xl md:text-6xlmd:text-left font-semibold pb-4">Teaching Tech Lit</h1>
-			<h2 class="text-2xl md:text-left py-4 font-thin px-2 ">
-				A guided introduction to the internet for adults looking to learn tech intuition and
-				literacy.
-			</h2>
-			<button
-				class=" bg-transparent mt-4 py-2 px-4 border border-white text-white rounded w-52 text-2xl hover:bg-white hover:text-[#3549A6] font-normal"
-				onclick="location.href='/buttons'"
-			>
-				Click Here to Get Started &#10140;
+	<section class="h-[5vh] bg-[#3549A6] m-auto rounded-lg shadow mt-8"></section>
+	<section class="flex flex-wrap justify-center items-center gap-x-10 m-auto p-8 h-[50vh] border-2 rounded-lg
+	my-8 shadow">
+		<section class="rounded-lg p-8">
+			<h1 class="text-6xl text-slate-800 font-semibold">Teaching Tech Lit<span class="text-[#3549A6]">.</span></h1>
+			<p class="pt-3.5 lg:max-w-[47ch] text-xl text-slate-600">A guided introduction to the internet for adults looking to learn tech intuition and literacy.</p>
+			<button class="bg-[#3549A6] text-xl font-semibold text-white py-2 px-4 rounded mt-3.5">
+				Get Started <span>&#10148;</span>
 			</button>
-		</div>
-		<div class="flex flex-col justify-end items-end">
-			<img src="/assets/T_Logo_White.png" alt="the Teaching Tech Lit logo." />
-		</div>
-	</div>
-</div>
-<!-- What is teaching tech lit section -->
-<div class="content-area">
-	<div class="my-14">
-		<div class="flex flex-col">
-			<h1 class="text-5xl md:text-6xlmd:text-left font-semibold pb-3" style="color: #3549A6;">
-				What is Teaching Tech Lit?
-			</h1>
-
-			<div class="mr-32">
-				<p class="text-xl font-thin pt-3">
-					Current technological literacy programs are for young children (6-12) or if it is for
-					adults it is only job skills training. We are instead looking to teach adults tech
-					intuition and literacy, rather than focusing on programs like Office or Adobe. Older
-					people who did not grow up with computers and tech face the biggest gap in technological
-					literacy.
-				</p>
-			</div>
-		</div>
-	</div>
-
-	<hr />
-	<!-- Lessons section -->
-	<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mt-10 pb-3" style="color: #3549A6;">
-		Lessons
-	</h1>
-	<div class="grid lg:grid-cols-3 mt-8 mb-10">
-		<div class="cards rounded overflow-hidden shadow border py-2 ">
-			<div class="">
-				<div class="font-bold text-3xl py-4 mb-2 text-gray-800">Browsers</div>
-				<img src="/assets/Edge_logo.png" alt="edge logo" class="responsive pb-4" />
-			</div>
-			<div class="py-2">
-				<span
-					class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700"
-					>Not Completed</span
-				>
-			</div>
-			<button
-				class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600"
-				>Start</button
-			>
-		</div>
-		<div class="cards rounded overflow-hidden shadow border py-2 lg:my-0 my-4">
-			<div class="lg:h-full">
-				<div class="font-bold text-3xl py-4 mb-2 text-gray-800 ">Icons</div>
-				<Settings size="64" class="responsive" />
-			</div>
-			<div class="py-2">
-				<span
-					class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700"
-					>Not Completed</span
-				>
-			</div>
-			<button
-				class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600"
-				onclick="location.href='/icons'">Start</button
-			>
-		</div>
-		<div class="cards rounded overflow-hidden shadow border py-2 lg:my-0 my-4">
-			<div class="">
-				<div class="font-bold text-3xl py-4 mb-2 text-gray-800">Devices</div>
-				<img src="/assets/windows_logo.png" alt="Windows logo" class="responsive pb-4" />
-			</div>
-			<div class="py-2">
-				<span
-					class="inline-block px-4 bg-gray-200 rounded-full text-base font-semibold text-gray-700"
-					>Not Completed</span
-				>
-			</div>
-			<button
-				class="p-2 border card-btn text-2xl text-white rounded hover:bg-blue-600 hover:border-blue-600"
-				onclick="location.href='/devices'">Start</button
-			>
-		</div>
-	</div>
-</div>
+		</section>
+		<aside>
+			<img src="assets/educator.svg" alt="Educators svg">
+		</aside>
+	</section>
+	<section class="flex flex-wrap justify-center items-center m-auto p-6 gap-x-2 rounded-lg border-2 mb-8">
+		<aside>
+			<img src="assets/attention.svg" alt="Attention SVG">
+		</aside>
+		<section class="p-8">
+			<h3 class="text-3xl text-slate-800 font-semibold">What Is Teaching Tech Lit<span class="text-[#3549A6]">?</span></h3>
+			<p class="text-base lg:max-w-[70ch] text-slate-600 pt-1.5 ">Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.</p>
+		</section>
+	</section>
+</main>
