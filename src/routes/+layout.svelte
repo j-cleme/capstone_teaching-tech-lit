2b8a16dfb480da61001a2be54079fb8b997e668c
jcleme <clemensjosh@protonmail.com>
2023-03-02 15:45:00 -0500

    finished desktop nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,7 @@
 </script>
 
 <nav
-	id="nav"
-	class="w-11/12 m-auto mb-0 flex justify-between items-center py-8 h-18 md:h-12 text-sm font-semibold border-b "
+	class="w-11/12 m-auto mb-0 flex justify-between items-center py-8 h-18 md:h-12 text-sm font-semibold border-b"
 >
 	<div on:click={toggleMobileNav} on:keypress={toggleMobileNav} class="md:hidden">
 		<Menu
@@ -20,56 +19,37 @@
 			class="mx-2 my-2 duration-75 text-neutral-600 {navVisibleState ? 'rotate-90' : ''} "
 		/>
 	</div>
-	{#if navVisibleState}
-		<a on:click={toggleMobileNav} on:keypress={toggleMobileNav} href="/" class="hover:bg-white">
-			<img src="/assets/logo.png" alt="Teaching Tech Lit logo" class="h-6 w-6 " />
-			<h1 class="mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900 ">Teaching Tech Lit</h1>
-		</a>
-	{/if}
-	{#if !navVisibleState}
-		<a
-			href="/"
-			class="flex flex-row w-full m-0 justify-start items-center col-start-7 col-end-13 md:col-start-1 md:col-end-3"
-		>
-			<img src="/assets/logo.png" alt="Teaching Tech Lit logo" class="h-6 w-6" />
-			<h1 class="mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900">Teaching Tech Lit</h1>
-		</a>
-	{/if}
-	<a
-		class="link group px-2 py-2 row-start-3 md:row-start-1 col-span-12 md:col-span-1 {navVisibleState
-			? ''
-			: 'hidden md:inline'} md:row-start-1 md:col-start-8"
-		href="/buttons"
-		>Buttons
+	<a href="/" class="group flex flex-row w-full m-0 justify-start items-center">
+		<img src="/assets/logo.png" alt="Teaching Tech Lit logo" class="h-6 w-6" />
+		<h1 class="group mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900">
+			Teaching Tech Lit<span
+				class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
+			/>
+		</h1>
+	</a>
+	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/buttons"
+		>Lesson 1
 		<span
 			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
 		/>
 	</a>
-	<a
-		class="link px-2 py-2  hover:rounded-full hover:text-white  row-start-4 md:row-start-1 col-span-12 md:col-span-1 {navVisibleState
-			? ''
-			: 'hidden md:inline'} md:row-start-1 md:col-start-9"
-		href="/icons"
-		>Icons
+	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/buttons"
+		>Lesson 2
+		<span
+			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
+		/>
 	</a>
-	<a
-		class="link px-2 py-2  hover:rounded-full hover:text-white  row-start-5 md:row-start-1 col-span-12 md:col-span-1 {navVisibleState
-			? ''
-			: 'hidden md:inline'} md:row-start-1 md:col-start-10"
-		href="/browsers"
-		>Browsers
+	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/buttons"
+		>Lesson 3
+		<span
+			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
+		/>
 	</a>
-	<a
-		class="link px-2 py-2  hover:rounded-full hover:text-white row-start-6 md:row-start-1 col-span-12 md:col-span-1 {navVisibleState
-			? ''
-			: 'hidden md:inline'} md:row-start-1 md:col-start-11"
-		href="/devices">Devices</a
-	>
-	<a
-		class="link px-2 py-2 hover:rounded-full hover:text-white  row-start-7 md:row-start-1 col-span-12 md:col-span-1 {navVisibleState
-			? ''
-			: 'hidden md:inline'} md:row-start-1 md:col-start-12"
-		href="/safety">Safety</a
-	>
+	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/buttons"
+		>Lesson 4
+		<span
+			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
+		/>
+	</a>
 </nav>
 <slot />
