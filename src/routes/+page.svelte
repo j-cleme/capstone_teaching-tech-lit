aa7e2be9868b85c7b7002999e998cfe894609942
jcleme <clemensjosh@protonmail.com>
2023-03-02 15:45:16 -0500

    fixed screen size related layout issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,40 @@
-<main>
-
-	<section class="h-[5vh] bg-[#3549A6] m-auto rounded-lg shadow mt-8"></section>
-	<section class="flex flex-wrap justify-center items-center gap-x-10 m-auto p-8 h-[50vh] border-2 rounded-lg
-	my-8 shadow">
+<main class="flex flex-col">
+	<section
+		class="flex flex-wrap justify-center items-center gap-x-10 m-auto p-8 min-h-[50vh] max-h-screen border-2 rounded-lg
+	my-8 shadow"
+	>
 		<section class="rounded-lg p-8">
-			<h1 class="text-6xl text-slate-800 font-semibold">Teaching Tech Lit<span class="text-[#3549A6]">.</span></h1>
-			<p class="pt-3.5 lg:max-w-[47ch] text-xl text-slate-600">A guided introduction to the internet for adults looking to learn tech intuition and literacy.</p>
+			<h1 class="text-6xl text-slate-800 font-semibold">
+				Teaching Tech Lit<span class="text-[#3549A6]">.</span>
+			</h1>
+			<p class="pt-3.5 lg:max-w-[47ch] text-xl text-slate-600">
+				A guided introduction to the internet for adults looking to learn tech intuition and
+				literacy.
+			</p>
 			<button class="bg-[#3549A6] text-xl font-semibold text-white py-2 px-4 rounded mt-3.5">
 				Get Started <span>&#10148;</span>
 			</button>
 		</section>
 		<aside>
-			<img src="assets/educator.svg" alt="Educators svg">
+			<img src="assets/educator.svg" alt="Educators svg" />
 		</aside>
 	</section>
-	<section class="flex flex-wrap justify-center items-center m-auto p-6 gap-x-2 rounded-lg border-2 mb-8">
+	<section
+		class="flex flex-wrap justify-center items-center m-auto p-6 gap-x-2 rounded-lg border-2 mb-8"
+	>
 		<aside>
-			<img src="assets/attention.svg" alt="Attention SVG">
+			<img src="assets/attention.svg" alt="Attention SVG" />
 		</aside>
 		<section class="p-8">
-			<h3 class="text-3xl text-slate-800 font-semibold">What Is Teaching Tech Lit<span class="text-[#3549A6]">?</span></h3>
-			<p class="text-base lg:max-w-[70ch] text-slate-600 pt-1.5 ">Current technological literacy programs are for young children (6-12) or if it is for adults it is only job skills training. We are instead looking to teach adults tech intuition and literacy, rather than focusing on programs like Office or Adobe. Older people who did not grow up with computers and tech face the biggest gap in technological literacy.</p>
+			<h3 class="text-3xl text-slate-800 font-semibold">
+				What Is Teaching Tech Lit<span class="text-[#3549A6]">?</span>
+			</h3>
+			<p class="text-base lg:max-w-[70ch] text-slate-600 pt-1.5 ">
+				Current technological literacy programs are for young children (6-12) or if it is for adults
+				it is only job skills training. We are instead looking to teach adults tech intuition and
+				literacy, rather than focusing on programs like Office or Adobe. Older people who did not
+				grow up with computers and tech face the biggest gap in technological literacy.
+			</p>
 		</section>
 	</section>
 </main>
