5bc3c279e27d347d4b6cec4c49690bfa215a6c7f
jcleme <clemensjosh@protonmail.com>
2023-03-02 17:34:05 -0500

    delete about page

--- src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<main>
-	<h1>About Us</h1>
-	<p>
-		Current technological literacy programs are for young children (6-12) or if it is for adults it
-		is only job skills training. We are instead looking to teach adults tech intuition and literacy,
-		rather than focusing on programs like Office or Adobe. Older people who did not grow up with
-		computers and tech face the biggest gap in technological literacy.
-	</p>
-</main>
