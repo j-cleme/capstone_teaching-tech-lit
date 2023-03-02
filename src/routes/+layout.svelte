fe6ece8acff379e918b01105a3c1f8012c0d8d68
jcleme <clemensjosh@protonmail.com>
2023-03-02 16:33:49 -0500

    preview of lesson completion indicators

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import { Menu } from 'lucide-svelte';
+	import { CheckCircle2, Menu, XCircle } from 'lucide-svelte';
 
 	let navVisibleState: Boolean = false;
 
 	function toggleMobileNav(): void {
 		navVisibleState = navVisibleState ? false : true;
-		console.log(navVisibleState);
+		// console.log(navVisibleState);
 	}
 </script>
 
@@ -19,7 +19,7 @@
 			class="mx-2 my-2 duration-75 text-neutral-600 {navVisibleState ? 'rotate-90' : ''} "
 		/>
 	</div>
-	<a href="/" class="group flex flex-row w-full m-0 justify-start items-center">
+	<a href="/" class="group flex flex-row w-full m-0 md:justify-start items-center">
 		<img src="/assets/logo.png" alt="Teaching Tech Lit logo" class="h-6 w-6" />
 		<h1 class="group mx-1 md:px-2 py-2 text-lg font-semibold text-neutral-900">
 			Teaching Tech Lit<span
@@ -28,13 +28,19 @@
 		</h1>
 	</a>
 	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/devices"
-		>Lesson 1
+		>Lesson 1 <span class="inline-block stroke-red-600"
+			><XCircle size={14} color="stroke-red-600" />
+			<!-- <CheckCircle2 size={14} color="stroke-emerald-800"  /> -->
+		</span>
 		<span
 			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
 		/>
 	</a>
 	<a class="group px-2 py-2 min-w-max {navVisibleState ? '' : 'hidden md:inline'}" href="/icons"
-		>Lesson 2
+		>Lesson 2<span class="inline-block stroke-emerald-500">
+			<!-- <XCircle size={14} color="stroke-red-600" /> -->
+			<CheckCircle2 size={14} color="stroke-emerald-500" />
+		</span>
 		<span
 			class="block bg-[#3549A6] max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5"
 		/>
