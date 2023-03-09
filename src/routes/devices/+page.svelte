49ef2f5737918b1982ab9b95fa59356bf280d17b
jcleme <clemensjosh@protonmail.com>
2023-03-08 20:49:30 -0500

    re-added next lesson button

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -1,152 +1,63 @@
+<script>
+	import { Lightbox, LightboxGallery, GalleryThumbnail, GalleryImage } from 'svelte-lightbox';
+
+	let lightboxProgrammaticController;
+</script>
+
 <svelte:head>
 	<title>Lesson 1 - Devices</title>
 </svelte:head>
-<script>
-    import { 
-        Lightbox,
-        LightboxGallery,
-        GalleryThumbnail,
-        GalleryImage
-    } from 'svelte-lightbox'
-    
-    let lightboxProgrammaticController
-</script>
-<style>
-	
-
-
-</style>
 <main>
-	<section class="flex flex-wrap justify-center items-center gap-x-10 p-8 w-fit
-	m-auto" role="banner">
-		<section class="rounded p-8">
+	<section
+		class="flex flex-wrap justify-center items-center gap-x-10 p-8 w-fit
+	m-auto"
+		role="banner"
+	>
+		<div class="rounded p-8">
 			<h1 class="text-6xl text-slate-800 font-semibold">
 				Lesson 1 <span class="text-[#3549A6]">-</span> Devices
 			</h1>
 			<p class="pt-3.5 lg:max-w-[47ch] text-xl text-slate-600 mb-3.5">
-				All devices are different, but some functions are seen across all platforms. 
-				Let's explore them.
+				All devices are different, but some functions are seen across all platforms. Let's explore
+				them.
 			</p>
-		</section>
+		</div>
 		<aside>
-			<img src="assets/devices/devices.svg" alt="Devices Hero Image">
+			<img src="assets/devices/devices.svg" alt="Devices Hero" />
 		</aside>
 	</section>
-	<hr class=" w-4/5 m-auto ">
+	<hr class=" w-4/5 m-auto " />
 	<section class=" p-8 flex flex-wrap justify-center items-center">
-		<section class=" mr-14">
-			<h2 class="text-4xl text-slate-800 font-semibold">
-				Taskbars
-			</h2>
+		<div class=" mr-14">
+			<h2 class="text-4xl text-slate-800 font-semibold">Taskbars</h2>
 			<p class="text-base text-slate-600 w-[50ch]">
-				Almost all devices have a top and/or bottom bar, these allow you to access things you need often quickly and easily.
+				Almost all devices have a top and/or bottom bar, these allow you to access things you need
+				often quickly and easily.
 			</p>
-		</section>
-		<section class="mx-4 border-r px-4">
+		</div>
+		<div class="mx-4 border-r px-4">
 			<Lightbox description="MacOS Desktop" style="max-width: !important none;">
-				<img src="assets/devices/MacOS_desktop.jpg" alt="MacOS desktop" class=" m-auto mb-4 w-100 h-100">
+				<img
+					src="assets/devices/MacOS_desktop.jpg"
+					alt="MacOS desktop"
+					class=" m-auto mb-4 w-100 h-100"
+				/>
 			</Lightbox>
 			<p class="text-base text-slate-600 w-[55ch] text-center">
-				For MacOS there is both a top and bottom bar, the top bar is a menu and the bottom bar is used to hold your computer's apps.
+				For MacOS there is both a top and bottom bar, the top bar is a menu and the bottom bar is
+				used to hold your computer's apps.
 			</p>
-		</section>
-		<section class="mx-4 px-4">
+		</div>
+		<div class="mx-4 px-4">
 			<Lightbox description="Windows 10 Desktop" style="max-width: !important none;">
-				<img src="assets/devices/windows_desktop.png" alt="Windows desktop" class=" m-auto mb-4">
-			</Lightbox>			
+				<img src="assets/devices/windows_desktop.png" alt="Windows desktop" class=" m-auto mb-4" />
+			</Lightbox>
 			<p class="text-base text-slate-600 w-[55ch] text-center">
-				Windows only has a bottom bar that holds your open and most used apps as well as the start button.
+				Windows only has a bottom bar that holds your open and most used apps as well as the start
+				button.
 			</p>
-		</section>
+		</div>
 	</section>
-</main>
-
-<!--<div class="bg-[#3549A6]">
-	<div class="content-area">
-		<h1 class="text-white" id="Icons">Lesson 1 - Devices</h1>
-	</div>
-</div>
-<div class="content-area">
-	<div class="mt-10 mb-16">
-		<p class="text-xl font-thin">
-			All devices are different, but some functions are seen across all platforms. Almost all
-			devices have a top and/or bottom bar, these allow you to access things you need often quickly
-			and easily.
-		</p>
-		<br />
-		<div class="grid grid-cols lg:grid-cols-2">
-			<div class="m-auto">
-				<div class="cards rounded overflow-hidden shadow border p-6 h-full">
-					<p class="text-xl font-semibold">
-						For Apple there is both a top and bottom bar, the top bar is a menu and the bottom bar
-						is used to hold your computer's apps:
-					</p>
-					<br />
-					<img src="/assets/MacOS_desktop.jpg" alt="MacOS Desktop" class="m-auto" />
-					<p class="mt-2 text-sm font-semibold">MacOS - Desktop</p>
-				</div>
-			</div>
-
-			<div class="m-auto">
-				<div class="cards rounded overflow-hidden shadow border p-6 h-full">
-					<p class="text-xl font-semibold">
-						Windows only has a bottom bar that holds your open and most used apps as well as the
-						start button, which we will expand on.
-					</p>
-					<br />
-					<img src="/assets/windows_desktop.png" alt="Windows Desktop" class="m-auto" />
-					<p class="mt-2 text-sm font-semibold">Windows 11 - Desktop</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<hr />
-	<div class="mt-10 mb-4">
-		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mb-2" style="color: #3549A6;">
-			Start Buttons
-		</h1>
-		<p class="text-xl font-thin mt-4 mb-6">
-			A start button can be used to search a device for programs or files, its look and location
-			differ depending on device.
-		</p>
-		<div class="grid grid-cols lg:grid-cols-3">
-			<div class="flex flex-col text-center w-72 rounded overflow-hidden shadow border p-6 h-full">
-				<p class="text-xl font-semibold mt-4 mb-6">MacOS</p>
-				<img src="assets/apple_logo.png" alt="Apple logo" class="m-auto w-2/6 mb-8" />
-				<p class="text-sm font-semibold m-auto">Located - Top Left</p>
-			</div>
-			<div class="flex flex-col text-center w-72 rounded overflow-hidden shadow border p-6">
-				<p class="text-xl font-semibold mt-4 mb-6">Windows</p>
-				<img src="assets/windows_logo.png" alt="Apple logo" class="m-auto w-2/6 mb-8" />
-				<p class="text-sm font-semibold m-auto">Located - Bottom Left or Center</p>
-			</div>
-		</div>
-	</div>
-	<hr />
-	<div class="mt-10 mb-16">
-		<h1 class="text-5xl md:text-6xlmd:text-left font-semibold mb-2" style="color: #3549A6;">
-			File Explorers
-		</h1>
-		<p class="text-xl font-thin mt-4 mb-6">
-			Every device will have a file explorer that allows you to search for and organize your files.
-		</p>
-		<div class="grid grid-cols lg:grid-cols-2">
-			<div class="cards rounded overflow-hidden shadow border p-6 h-full">
-				<p class="text-xl font-semibold mt-4 mb-6">
-					On MacOS you have Finder which looks like this:
-				</p>
-				<img src="assets/Finder.png" alt="MacOS Finder" class="m-auto" />
-				<p class="text-sm font-semibold m-auto">MacOS Ventura - Finder</p>
-			</div>
-			<div class="cards rounded overflow-hidden shadow border p-6 h-full">
-				<p class="text-xl font-semibold mt-4 mb-6">
-					On Windows you have File Explorer which looks like this:
-				</p>
-				<img src="assets/File_Explorer.png" alt="Windows File Explorer" class="m-auto" />
-				<p class="text-sm font-semibold mt-6 m-auto">Windows 11 - File Explorer</p>
-			</div>
-		</div>
-	</div>
 	<section class="flex items-center justify-center my-5">
 		<a
 			href="/icons"
@@ -155,4 +66,4 @@
 			Lesson 2 <span>&#10148;</span>
 		</a>
 	</section>
-</div>-->
+</main>
