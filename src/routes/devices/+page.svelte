6ac4292c95a22e146a9c39af7c9a54440af5a539
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-14 02:50:42 -0400

    Fixes to devices

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -1,16 +1,6 @@
 <svelte:head>
 	<title>Lesson 1 - Devices</title>
 </svelte:head>
-<script>
-    import { 
-        Lightbox,
-        LightboxGallery,
-        GalleryThumbnail,
-        GalleryImage
-    } from 'svelte-lightbox'
-    
-    let lightboxProgrammaticController
-</script>
 <style>
 	
 
@@ -31,7 +21,7 @@
 
 	</section>
 	<hr class=" w-4/5 m-auto ">
-	<section class=" p-8 flex flex-row justify-center items-center w-100">
+	<section class="grid grid-cols-3">
 		<section class=" mr-12">
 			<h2 class="text-4xl text-slate-800 font-semibold">
 				Taskbars
@@ -42,13 +32,13 @@
 		</section>
 		<section class="border-r flex flex-col pr-8 items-center">
 				<img src="assets/devices/MacOS_desktop.jpg" alt="MacOS desktop" class=" m-auto mb-4 w-100 h-100">
-			<p class="text-base text-slate-600 w-[55ch] text-center">
+			<p class="text-base text-slate-600 w-[45ch] text-center">
 				For MacOS there is both a top and bottom bar, the top bar is a menu and the bottom bar is used to hold your computer's apps.
 			</p>
 		</section>
 		<section class="ml-8 flex flex-col items-center">
 				<img src="assets/devices/windows_desktop.png" alt="Windows desktop" class=" m-auto mb-4">		
-			<p class="text-base text-slate-600 w-[55ch] text-center">
+			<p class="text-base text-slate-600 w-[45ch] text-center">
 				Windows only has a bottom bar that holds your open and most used apps as well as the start button.
 			</p>
 		</section>
