7bfaf950edb6a4ac5a9d3886ef0c922bf2dd0f0d
jcleme <clemensjosh@protonmail.com>
2023-03-22 17:36:38 -0600

    fixed button for last lesson page

--- src/routes/NextLesson.svelte
+++ src/routes/NextLesson.svelte
@@ -5,6 +5,10 @@
 	export let nextPageLink: string;
 
 	let nextLessonNumber: number = +lessonNumber + 1;
+	let textDisplay: string = 'Lesson ' + nextLessonNumber;
+	if (nextLessonNumber == 6) {
+		textDisplay = 'Back to Home';
+	}
 
 	function complete() {
 		lessonTracking.update((value: string) => value + lessonNumber);
@@ -16,5 +20,5 @@
 	href={nextPageLink}
 	class="bg-[#3549A6] text-xl font-semibold text-white py-2 px-4 rounded border-b-4 border-blue-900 border-r-4"
 >
-	Lesson {nextLessonNumber} <span>&#10148;</span>
+	{textDisplay} <span>&#10148;</span>
 </a>
