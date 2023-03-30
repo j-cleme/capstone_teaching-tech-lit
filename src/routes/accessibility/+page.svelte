a7c26827ebf4393b48cde0e136969e66784472d0
Jay-4551 <106278277+Jay-4551@users.noreply.github.com>
2023-03-30 13:43:22 -0400

    Accessibility Updates

--- src/routes/accessibility/+page.svelte
+++ src/routes/accessibility/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { ChevronDown,ZoomIn,Type } from 'lucide-svelte';
+	import { ChevronDown,ZoomIn,Type,Palette,Ear,MousePointer2,Keyboard } from 'lucide-svelte';
 	import NextLesson from '$lib/NextLesson.svelte';
 	import { base } from '$app/paths';
 </script>
@@ -32,9 +32,15 @@
 			<ChevronDown size={50} style="color:rgb(71, 85, 105);" />
 		</div>
 	</div>
+	<h2 class="text-slate-800 text-4xl font-semibold mt-8">
+		Accessibility Options<span class="text-[#3549A6]">.</span>
+	</h2>
+	<p class="text-slate-600 my-4 text-lg">
+		Many devices and applications offer a range of options that can help users customize their experience and make it easier to use.
 
-	<div class="md:grid md:grid-cols-3 w-full justify-items-center items-center my-6">
-		<div>
+	</p>
+	<div class="md:grid md:grid-cols-2 w-full justify-items-center items-center my-2">
+		<div class="my-4">
 			<ZoomIn size={50} />
 			<h1 class=" text-2xl text-slate-800 font-semibold ">Zoom</h1>
 			<p class="text-slate-600 max-w-[45ch]">
@@ -42,7 +48,15 @@
 			</p>
 		</div>
 
-		<div>
+		<div class="my-4">
+			<Ear size={50} />
+			<h1 class=" text-2xl text-slate-800 font-semibold ">Narrator</h1>
+			<p class="text-slate-600 max-w-[45ch]">
+				The narrator feature gives you an audio description of what is currently happening on the screen. It is used for things such as reading out text or describing actions. This can be useful for people with visual impairments or those who prefer to listen rather than read.
+			</p>
+		</div>
+
+		<div class="my-4">
 			<Type size={50} />
 			<h1 class=" text-2xl text-slate-800 font-semibold ">Adjustable Text Size</h1>
 			<p class="text-slate-600 max-w-[45ch]">
@@ -50,31 +64,31 @@
 			</p>
 		</div>
 
-		<div>
-			<Type size={50} />
+		<div class="my-4">
+			<Palette size={50} />
 			<h1 class=" text-2xl text-slate-800 font-semibold ">Color Filters</h1>
 			<p class="text-slate-600 max-w-[45ch]">
-				This feature allows you to adjust the size of the text on the screen to make it bigger or smaller, depending on your preference. It is particularly useful for people with visual impairments or those who prefer larger text. 
+				Color filters are used to adjust the color scheme of a screen to make it easier to see and more comfortable for your eyes. It is most useful for people with color blindness, low vision, or sensitivity to certain colors
 			</p>
 		</div>
+
+		<div class="my-4">
+			<MousePointer2 size={50} />
+			<h1 class=" text-2xl text-slate-800 font-semibold ">Cursor Size</h1>
+			<p class="text-slate-600 max-w-[45ch]">
+				This allows you to adjust the size of the cursor to make it easier to see and track. 
+			</p>
+		</div>
+
+		<div class="my-4">
+			<Keyboard size={50} />
+			<h1 class=" text-2xl text-slate-800 font-semibold ">Sticky Keys</h1>
+			<p class="text-slate-600 max-w-[45ch]">
+				This allows the user to press one key at a time for keyboard shortcuts, rather than needing to press multiple keys at once. 			</p>
+		</div>
+
+
 	</div>
-	<p>
-		Blurb about the options with accessibility and what it is called(ease of access in settings on
-		windows)
-	</p>
-	<p>
-		Zoom in - many web browsers have built in zoom capabilities, these can be found in the settings
-		on the browser, signified by a magnifying glass. If you are on a laptop with a trackpad you can
-		change the zoom by putting two fingers on the pad, and moving them away from each other to zoom
-		in. To zoom out with this method put two fingers on the trackpad and bring your fingers
-		together.
-	</p>
-	<p>Adjustable text size</p>
-	<p>Color filters</p>
-	<p>Cursor size</p>
-	<p>Motion</p>
-	<p>Narrator</p>
-	<p>Sticky keys</p>
 </main>
 <section class="flex items-center justify-center my-5">
 	<NextLesson lessonNumber={'3'} nextPageLink="/browsers" />
