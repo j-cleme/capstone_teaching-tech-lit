a73ca266c49844945d3f7e8fbaced4abb0e702e0
jcleme <clemensjosh@protonmail.com>
2023-03-30 16:56:14 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	build: {
 		rollupOptions: {
-			external: './node_modules/svelte-modals/index.js'
+			external: './node_modules/svelte-modals/Modals.svelte'
 		}
 	},
 	plugins: [sveltekit()]
