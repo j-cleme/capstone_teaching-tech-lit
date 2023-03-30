221f7031c81388b6b37126504e6ff6d3419d49c6
jcleme <clemensjosh@protonmail.com>
2023-03-30 17:06:43 -0400

    externalize svelte-modals

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	build: {
-		rollupOptions: {
-			external: 'svelte-modals'
-		}
-	},
 	plugins: [sveltekit()]
 };
 
