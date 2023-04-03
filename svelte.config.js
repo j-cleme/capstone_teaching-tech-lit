c079edd86babdadbf80490b6f7d254e5ac1e85f1
jcleme <clemensjosh@protonmail.com>
2023-04-03 09:31:16 -0400

    change adapter
    build succeeded

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    }),
+    adapter: adapter(),
     paths: {
       assets: '',
       base: '/teaching-tech-lit',
