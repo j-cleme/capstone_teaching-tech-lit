9f066ae0e6029e88543212ce3bef0fb9a1c68c78
jcleme <clemensjosh@protonmail.com>
2023-04-04 14:03:23 -0400

    Buttons standardized sitewide

--- src/lib/NextLesson.svelte
+++ src/lib/NextLesson.svelte
@@ -19,7 +19,7 @@
 <a
 	on:click={complete}
 	href={base + nextPageLink}
-	class="bg-[#3549A6] text-xl font-semibold text-white py-2 px-4 rounded border-b-4 border-blue-900 border-r-4"
+	class="group inline-flex whitespace-nowrap rounded-md bg-[#ec598a] text-xl font-semibold text-white py-2 px-3 -translate-x-1 -translate-y-1 hover:translate-x-0 hover:translate-y-0 items-center justify-center transition duration-100"
 >
-	{textDisplay} <span>&#10148;</span>
+	{textDisplay} <span class="pl-2">&#10148;</span>
 </a>
