01b604582effa95944b3a3e9e5ceea9fb64bf911
jcleme <clemensjosh@protonmail.com>
2023-04-18 12:38:49 -0400

    change password input type to password

--- src/routes/safety/+page.svelte
+++ src/routes/safety/+page.svelte
@@ -49,7 +49,7 @@
 				<input
 					class="placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 pl-14 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm h-[60px] md:w-[450px]"
 					placeholder="Type a password here..."
-					type="text"
+					type="password"
 					name="search"
 				/>
 			</label>
